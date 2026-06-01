3cf3a6040de6a91279d0b94039bc3988b16e0998
KotonoSora <26807417+KotonoSora@users.noreply.github.com>
2026-06-01 12:59:26 +0700

    feat: 🎸 change version code and name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ configure<ApplicationExtension> {
         applicationId = "com.jn.paxl"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
